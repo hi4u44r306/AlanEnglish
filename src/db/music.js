8b2bab08feb5c5aae1e72e14abd2608f87470987
Victor <hi4u44r306@gmail.com>
2022-07-15 20:12:21 +0800

    update db

--- src/db/music.js
+++ src/db/music.js
@@ -1,72 +1,6 @@
 let musicDB = [
   {
       id: 0,
-      bookname: "習作本1",
-      page:"P.1",
-      img: "headphone1.png",
-      lang: "習作本1",
-      timesPlayed: 0,
-      type: "1",
-      musicName: "Clarx - Shakedown [NCS Release].mp3"
-     
-  },
-  {
-      id: 1,
-      bookname: "習作本1",
-      page:"P.2",
-      img: "headphone1.png",
-      lang: "習作本1",
-      timesPlayed: 0,
-      type:"1",
-      musicName: "mixkit-games-worldbeat-466.mp3"
-     
-  },
-  {
-      id: 2,
-      bookname: "習作本1 ",
-      page: "P.3",
-      img: "headphone1.png",
-      lang: "習作本1",
-      type: "1",
-      timesPlayed: 0,
-      musicName: "ROY KNOX -  Over My Head (Feat. Mike Robert) [NCS Release].mp3"
-     
-  },
-  {
-      id: 3,
-      bookname: "習作本1",
-      page:"P.4",
-      img: "headphone1.png",
-      lang: "習作本1",
-      timesPlayed: 0,
-      type: "1",
-      musicName: "Subtact - Want You (feat. Sara Skinner) [NCS Release].mp3"
-      
-  },
-  {
-      id: 4,
-      bookname: "習作本1",
-      page:"P.5",
-      img: "headphone1.png",
-      lang: "習作本1",
-      timesPlayed: 0,
-      type: "1",
-      musicName: "Midranger - Apocalypse [NCS Release].mp3"
-      
-  },
-  {
-      id: 5,
-      bookname: "習作本1",
-      page:"P.6",
-      img: "headphone1.png",
-      lang: "習作本1",
-      timesPlayed: 0,
-      type: "1",
-      musicName: "N3WPORT - Alive (feat. Neoni) [NCS Release].mp3"
-      
-  },
-  {
-      id: 6,
       bookname: "習作本2",
       page:"P.4",
       img: "headphone2.png",
@@ -77,7 +11,7 @@ let musicDB = [
       
   },
   {
-      id: 7,
+      id: 1,
       bookname: "習作本2",
       page:"P.6",
       img: "headphone2.png",
@@ -88,453 +22,89 @@ let musicDB = [
       
   },
   {
-      id: 8,
+      id: 2,
       bookname: "習作本2",
-      page:"P.3",
+      page:"P.8",
       img: "headphone2.png",
       lang: "習作本2",
       timesPlayed: 0,
       type: "2",
-      musicName: "Heuse & Tom Wilson - Ignite [NCS Release].mp3"
+      musicName: "習作本2 P8.mp3"
      
   },
   {
-    id: 9,
+    id: 3,
     bookname: "習作本2",
-    page:"P.",
+    page:"P.10",
     img: "headphone2.png",
     lang: "習作本2",
     timesPlayed: 0,
     type: "2",
-    musicName: "習作本2 P4.mp3"
+    musicName: "習作本2 P10.mp3"
     
   },
   {
-    id: 10,
+    id: 4,
     bookname: "習作本2",
-    page:"P.",
+    page:"P.14",
     img: "headphone2.png",
     lang: "習作本2",
     timesPlayed: 0,
     type: "2",
-    musicName: "習作本2 P6.mp3"
+    musicName: "習作本2 P14.mp3"
    
 },
 {
-    id: 11,
-    bookname: "習作本3",
-    page:"P.1",
-    img: "headphone3.png",
-    lang: "習作本3",
+    id: 5,
+    bookname: "習作本2",
+    page:"P.16",
+    img: "headphone2.png",
+    lang: "習作本2",
     timesPlayed: 0,
-    type:"3",
-    musicName: "mixkit-games-worldbeat-466.mp3"
+    type:"2",
+    musicName: "習作本2 P16.mp3"
   
 },
 {
-    id: 12,
-    bookname: "習作本3",
-    page:"P.2",
-    img: "headphone3.png",
-    lang: "習作本3",
-    type: "3",
+    id: 6,
+    bookname: "習作本2",
+    page:"P.18",
+    img: "headphone2.png",
+    lang: "習作本2",
+    type: "2",
     timesPlayed: 0,
-    musicName: "ROY KNOX -  Over My Head (Feat. Mike Robert) [NCS Release].mp3"
+    musicName: "習作本2 P18.mp3"
 },
 {
-    id: 13,
-    bookname: "習作本3",
-    page:"P.3",
-    img: "headphone3.png",
-    lang: "習作本3",
+    id: 7,
+    bookname: "習作本2",
+    page:"P.26",
+    img: "headphone2.png",
+    lang: "習作本2",
     timesPlayed: 0,
-    type: "3",
-    musicName: "Subtact - Want You (feat. Sara Skinner) [NCS Release].mp3"
+    type: "2",
+    musicName: "習作本2 P26.mp3"
 },
 {
-    id: 14,
-    bookname: "習作本3",
-    page:"P.4",
-    img: "headphone3.png",
-    lang: "習作本3",
+    id: 8,
+    bookname: "習作本2",
+    page:"P.28",
+    img: "headphone2.png",
+    lang: "習作本2",
     timesPlayed: 0,
-    type: "3",
-    musicName: "Midranger - Apocalypse [NCS Release].mp3"
+    type: "2",
+    musicName: "習作本2 P28.mp3"
 },
 {
-    id: 15,
-    bookname: "習作本3",
-    page:"P.5",
-    img: "headphone3.png",
-    lang: "習作本3",
+    id: 9,
+    bookname: "習作本2",
+    page:"P.34",
+    img: "headphone2.png",
+    lang: "習作本2",
     timesPlayed: 0,
-    type: "3",
-    musicName: "N3WPORT - Alive (feat. Neoni) [NCS Release].mp3"
-},
-{
-    id: 16,
-    bookname: "習作本4",
-    page:"P.1",
-    img: "headphone4.png",
-    lang: "習作本4",
-    timesPlayed: 0,
-    type:"4",
-    musicName: "impact.mp3"
-},
-{
-    id: 17,
-    bookname: "習作本4",
-    page:"P.2",
-    img: "headphone4.png",
-    lang: "習作本4",
-    timesPlayed: 0,
-    type:"4",
-    musicName: "impact2.mp3"
-},
-{
-    id: 18,
-    bookname: "習作本4",
-    page:"P.3",
-    img: "headphone4.png",
-    lang: "習作本4",
-    timesPlayed: 0,
-    type: "4",
-    musicName: "Heuse & Tom Wilson - Ignite [NCS Release].mp3"
-},
-{
-  id: 19,
-  bookname: "習作本4",
-  page:"P.4",
-  img: "headphone4.png",
-  lang: "習作本4",
-  timesPlayed: 0,
-  type: "4",
-  musicName: "Heuse & Tom Wilson - Ignite [NCS Release].mp3"
-},
-{
-    id: 20,
-    bookname: "習作本4",
-    page:"P.5",
-    img: "headphone4.png",
-    lang: "習作本4",
-    timesPlayed: 0,
-    type: "4",
-    musicName: "N3WPORT - Alive (feat. Neoni) [NCS Release].mp3"
-},
-{
-    id: 21,
-    bookname: "習作本5",
-    page:"P.1",
-    img: "headphone5.png",
-    lang: "習作本5",
-    timesPlayed: 0,
-    type:"5",
-    musicName: "impact.mp3"
-},
-{
-    id: 22,
-    bookname: "習作本5",
-    page:"P.2",
-    img: "headphone5.png",
-    lang: "習作本5",
-    timesPlayed: 0,
-    type:"5",
-    musicName: "impact2.mp3"
-},
-{
-    id: 23,
-    bookname: "習作本5",
-    page:"P.3",
-    img: "headphone5.png",
-    lang: "習作本5",
-    timesPlayed: 0,
-    type: "5",
-    musicName: "Heuse & Tom Wilson - Ignite [NCS Release].mp3"
-},
-{
-  id: 24,
-  bookname: "習作本5",
-  page:"P.4",
-  img: "headphone5.png",
-  lang: "習作本5",
-  timesPlayed: 0,
-  type: "5",
-  musicName: "Heuse & Tom Wilson - Ignite [NCS Release].mp3"
-},
-{
-  id: 25,
-  bookname: "習作本5",
-  page:"P.555",
-  img: "headphone5.png",
-  lang: "習作本5",
-  timesPlayed: 0,
-  type: "5",
-  musicName: "Heuse & Tom Wilson - Ignite [NCS Release].mp3"
+    type: "2",
+    musicName: "習作本2 P34.mp3"
 }
 ];
 
 export default musicDB;
-/*let musicDB = [
-{
-  id:0,
-  bookname:"Left Foot Up Right Foot Slide",
-  author_bookname: "Yung Nazty",
-  img: "left_foot_up_right_foot_slide.jpg",
-  lang: "ENGLISH",
-  timesPlayed:0,
-  musicName:"left_foot_up_right_foot_slide.mp3"
-},
-{
-  id:1,
-  bookname: "Intentions (feat. Quavo)",
-  author_bookname: "Justin Bieber",
-  img: "intentions.jpg",
-  lang: "ENGLISH",
-  timesPlayed:0,
-  musicName:"intentions.mp3"
-},
-{
-  id:2,
-  bookname: "Naina Da Kya Kasoor",
-  author_bookname: "Amit Trivedi",
-  img: "naina_da_kya_kasoor.jpg",
-  lang:"HINDI",
-  timesPlayed:0,
-  musicName:"Naina_da_kya_kasoor.mp3"
-},
-{
-  id:3,
-  bookname: "Señorita",
-  author_bookname: "Shawn Mendes",
-  img: "senorita.jpg",
-  lang: "ENGLISH",
-  timesPlayed:0,
-  musicName:"senorita.mp3"
-},
-{
-  id:4,
-  bookname: "Memories",
-  author_bookname: "Maroon 5",
-  img: "memories.jpg",
-  lang: "ENGLISH",
-  timesPlayed:0,
-  musicName:"memories.mp3"
-},
-{
-  id:5,
-  bookname: "Skechers",
-  author_bookname: "DripReport",
-  img: "skechers.jpg",
-  lang: "ENGLISH",
-  timesPlayed:0,
-  musicName:"Skechers.mp3"
-},
-{
-  id:6,
-  bookname: "Sunflower - Spider-Man: Into the Spider-Verse",
-  author_bookname: "Post Malone",
-  img: "sunflower.jpg",
-  lang: "ENGLISH",
-  timesPlayed:0,
-  musicName:"Sunflower.mp3"
-},
-{
-  id:7,
-  bookname: "I Can’t Get Enough",
-  author_bookname: "benny blanco, Selena Gomez, J Balvin, Tainy",
-  img: "i_cant_get_enough.jpg",
-  lang: "ENGLISH",
-  timesPlayed:0,
-  musicName:"i_cant_get_enough.mp3"
-},
-{
-  id:8,
-  bookname: "Old Town Road",
-  author_bookname: "Lil Nas X",
-  img: "old_town_road.jpg",
-  lang: "ENGLISH",
-  timesPlayed:0,
-  musicName:"old_town_road.mp3"
-},
-{
-  id:9,
-  bookname: "Beautiful Stillness",
-  author_bookname: "Billy-Joe",
-  img: "beautiful_stillness.jpg",
-  lang: "ENGLISH",
-  timesPlayed:0,
-  musicName:"Beautiful_Stillness.mp3"
-},
-{
-  id:10,
-  bookname: "death bed (coffee for your head)",
-  author_bookname: "Powfu",
-  img: "death_bed.jpg",
-  lang: "ENGLISH",
-  timesPlayed:0,
-  musicName:"death_bed.mp3"
-},
-{
-  id:11,
-  bookname: "Halka Halka (From \"Fanney Khan\")",
-  author_bookname: "Sunidhi Chauhan",
-  img: "halka_halka_suroor.jpg",
-  lang:"HINDI",
-  timesPlayed:0,
-  musicName:"halka_halka_suroor.mp3"
-},
-{
-  id:12,
-  bookname: "Kudi Nu Nachne De (From \"Angrezi Medium\")",
-  author_bookname: "Vishal Dadlani",
-  img: "kudi_nu_nachne_de.jpg",
-  lang:"HINDI",
-  timesPlayed:0,
-  musicName:"kudi_nu_nachne_de.mp3"
-},
-{
-  id:13,
-  bookname: "Illegal Weapon 2.0",
-  author_bookname: "Jasmine Sandlas",
-  img: "illeagal_weapon_2.jpg",
-  lang:"PUBJABI",
-  timesPlayed:0,
-  musicName:"illeagal_weapon_2.mp3"
-},
-{
-  id:14,
-  bookname: "Genda Phool (feat. Payal Dev)",
-  author_bookname: "Badshah",
-  img: "genda_phool.jpg",
-  lang:"BENGALI",
-  timesPlayed:0,
-  musicName:"genda_phool.mp3"
-},
-{
-  id:15,
-  bookname: "Yaar Mod Do",
-  author_bookname: "Guru Randhawa",
-  img: "yaar_mod_do.jpg",
-  lang:"PUNJABI",
-  timesPlayed:0,
-  musicName:"yaar_mod_do.mp3"
-},
-{
-  id:16,
-  bookname: "Shayad",
-  author_bookname: "Pritam",
-  img: "shayad.jpg",
-  lang:"HINDI",
-  timesPlayed:0,
-  musicName:"shayad.mp3"
-},
-{
-  id:17,
-  bookname: "Without Me",
-  author_bookname: "Halsey",
-  img: "without_me.jpg",
-  lang: "ENGLISH",
-  timesPlayed:0,
-  musicName:"without_me.mp3"
-},
-{
-  id:18,
-  bookname: "Buttabomma - Telugu",
-  author_bookname: "Armaan Malik",
-  img: "buttabomma.jpg",
-  lang:"TELUGU",
-  timesPlayed:0,
-  musicName:"ButtaBomma.mp3"
-},
-{
-  id:19,
-  bookname: "Coda",
-  author_bookname: "Jakob Ahlbom",
-  img: "coda.jpg",
-  lang: "ENGLISH",
-  timesPlayed:0,
-  musicName:"coda.mp3"
-},
-{
-  id:20,
-  bookname: "Khairiyat",
-  author_bookname: "Arijit Singh",
-  img: "khairiyat.jpg",
-  lang:"HINDI",
-  timesPlayed:0,
-  musicName:"khairiyat.mp3"
-},
-{
-  id:21,
-  bookname: "Haan Main Galat",
-  author_bookname: "Pritam",
-  img: "haan_mai_galat.jpg",
-  lang:"HINDI",
-  timesPlayed:0,
-  musicName:"haan_mai_galat.mp3"
-},
-{
-  id:22,
-  bookname: "Mere Liye Tum Kaafi Ho (From \"Shubh Mangal Zyada Saavdhan\")",
-  author_bookname: "Ayushmann Khurrana",
-  img: "mere_liiye_tum_kaafi_ho.jpg",
-  lang:"HINDI",
-  timesPlayed:0,
-  musicName:"mere_liiye_tum_kaafi_ho.mp3"
-},
-{
-  id:23,
-  bookname: "Fracture",
-  author_bookname: "Jakob Ahlbom",
-  img: "fracture.jpg",
-  lang: "ENGLISH",
-  timesPlayed:0,
-  musicName:"fracture.mp3"
-},
-{
-  id:24,
-  bookname: "Ghungroo (From \"War\")",
-  author_bookname: "Arijit Singh",
-  img: "ghungroo.jpg",
-  lang:"HINDI",
-  timesPlayed:0,
-  musicName:"ghungroo.mp3"
-},
-{
-  id:25,
-  bookname: "Happier",
-  author_bookname: "Marshmello",
-  img: "happier.jpg",
-  lang: "ENGLISH",
-  timesPlayed:0,
-  musicName:"happier.mp3"
-},
-{
-  id:26,
-  bookname: "Namo Namo",
-  author_bookname: "Amit Trivedi",
-  img: "namo_namo.jpg",
-  lang:"HINDI",
-  timesPlayed:0,
-  musicName:"namo_namo.mp3"
-},
-{
-  id:27,
-  bookname: "Faded",
-  author_bookname: "Alan Walker",
-  img: "faded.jpg",
-  lang: "ENGLISH",
-  timesPlayed:0,
-  musicName:"Faded_Alan_Walker.mp3"
-},
-{
-  id:28,
-  bookname: "I Don't Care (with Justin Bieber)",
-  author_bookname: "Ed Sheeran",
-  img: "i_dont_care.jpg",
-  lang: "ENGLISH",
-  timesPlayed:0,
-  musicName:"i_dont_care.mp3"
-}
-]
-export default musicDB;*/
