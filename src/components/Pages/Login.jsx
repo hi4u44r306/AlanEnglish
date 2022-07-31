d45a22e0ed6a8dada1270097d4e6ab79b2ae2b31
Victor <hi4u44r306@gmail.com>
2022-07-31 23:05:20 +0800

    smth

--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -57,7 +57,20 @@ class Login extends React.Component{
                         </div>
                        
                         <div className="main-row-text">
-                            <h1>Alan English</h1>
+                            <div className="loginbrand">
+                                <span>A</span>
+                                <span>L</span>
+                                <span>A</span>
+                                <span>N</span>
+                                <span> </span>
+                                <span>E</span>
+                                <span>N</span>
+                                <span>G</span>
+                                <span>L</span>
+                                <span>I</span>
+                                <span>S</span>
+                                <span>H</span>
+                            </div>
                             <p>系統化 | 口語化 | 聽力導向 </p>
                             <label>帳號</label>
                             <input
