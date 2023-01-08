4320226cf63aab6c8ae68a5c0cd743be616fceb5
Victor <hi4u44r306@gmail.com>
2023-01-08 22:13:08 +0800

    Fix the end of month doesn't refresh totaltime

--- src/components/fragment/FooterMusicPlayer.jsx
+++ src/components/fragment/FooterMusicPlayer.jsx
@@ -1,8 +1,8 @@
 import AudioPlayer, { RHAP_UI } from 'react-h5-audio-player'
-import React, {useRef, useState, useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {setCurrentPlaying} from "../../actions/actions";
-import { toast, ToastContainer} from "react-toastify"
+import React, { useRef, useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setCurrentPlaying } from "../../actions/actions";
+import { toast, ToastContainer } from "react-toastify"
 import Marquee from "react-fast-marquee";
 import firebase from 'firebase/app';
 import Name from "./Name";
@@ -10,11 +10,11 @@ import '../assets/scss/FooterPlayer.scss';
 import 'react-h5-audio-player/lib/styles.css';
 
 
-function FooterMusicPlayer({music}) {
+function FooterMusicPlayer({ music }) {
 
-    const [{ bookname, page , musicName}, setCurrTrack] = useState(music);
-    const {playlists} = useSelector(state => state.musicReducer);
-    const [currentuser,setCurrUser] = useState();
+    const [{ bookname, page, musicName }, setCurrTrack] = useState(music);
+    const { playlists } = useSelector(state => state.musicReducer);
+    const [currentuser, setCurrUser] = useState();
     const userId = firebase.auth().currentUser.uid;
     // const [totaltimeplayed,setTotaltimeplayed] = useState();
     // const currplayingmusicid = "'" + id + "'";
@@ -23,11 +23,11 @@ function FooterMusicPlayer({music}) {
     const audioElement = useRef();
     const currentDate = new Date().toJSON().slice(0, 10);
     const currentMonth = new Date().toJSON().slice(0, 7);
-    const userRef = db.collection('student').doc(currentuser); 
+    const userRef = db.collection('student').doc(currentuser);
 
-    const success = () =>  {
-        toast.success(`聽力次數 + 1`,{
-            className:"musicnotification",
+    const success = () => {
+        toast.success(`聽力次數 + 1`, {
+            className: "musicnotification",
             position: "top-center",
             autoClose: 1500,
             hideProgressBar: false,
@@ -36,202 +36,129 @@ function FooterMusicPlayer({music}) {
             draggable: true,
             progress: undefined,
             theme: "colored",
-            });
-        };
+        });
+    };
 
     useEffect(() => {
         setCurrTrack(music);
         // 每月1號重置所有播放次數//
-        
+
     }, [music]);
-        
+
 
     firebase.auth().onAuthStateChanged(user => { //從firestore取得student 集合中的登入中的useruid
-        if(user){
-            db.collection('student').onSnapshot(snapshot =>{
+        if (user) {
+            db.collection('student').onSnapshot(snapshot => {
                 setCurrUser(user.uid);
             });
-        }else{
+        } else {
             updatetimeplayedtofirestore();
         }
     });
 
-   
-
-    
-    
-    const updatetimeplayedtofirestore = () => {
-       
-//============================================================================================//
-
-            userRef.get().then((doc) =>{
-                if(doc.data().Resetallmusic === 'notupdated' || doc.data().Resetallmusic !== currentMonth+'alreadyupdated'){
-                    userRef.set({
-                        totaltimeplayed : 0,
-                        currdatetimeplayed : 0,
-                        Resetallmusic : currentMonth+'alreadyupdated',
-                    },{merge: true})
-                    firebase.database().ref('student/' + userId ).update({
-                        totaltimeplayed : 0,
-                    });
-                    // for(let i = 0; i < 601; i++){      
-                    //     let j = "'"+i+"'"
-                    //     userRef.collection('Musics').doc(j).set({ // 在特定User中加入Musics集合，在Musics中加入id以及timeplayed
-                    //         timeplayed : 0,
-                    //     })        
-                    // }
-                    console.log('First time reset')
-                }else{
-                    console.log('This account is alreadyreset')
-                }
-            }).catch(() =>{
-            })
-           
+    function updatetimeplayedtofirestore() {
 
 
-            const dbRef = firebase.database().ref();
-            
-            dbRef.child("student").child(userId).child("totaltimeplayed").get().then((snapshot) => {
-                const aaa = parseInt(snapshot.val())+1;
-                dbRef.child("student").child(userId).update({
-                    totaltimeplayed : aaa,
+        // Firestore
+        userRef.get().then((doc) => {
+            if (doc.data().Resetallmusic === 'notupdated' || doc.data().Resetallmusic !== currentMonth + 'alreadyupdated') {
+                userRef.set({
+                    totaltimeplayed: 0,
+                    currdatetimeplayed: 0,
+                    Resetallmusic: currentMonth + 'alreadyupdated',
+                }, { merge: true })
+                firebase.database().ref().child("student").child(userId).child("totaltimeplayed").update({
+                    totaltimeplayed: 0,
                 });
-            }).catch(() => {
-                firebase.database().ref('student/' + userId ).update({
-                    totaltimeplayed : 0,
-                });
-            });
-            
-
-
-            // 當使用者聽完一個音軌 推送Timestamp到firebase //
-            userRef.update({ 
-                onlinetime : currentDate,
-                onlinemonth : currentMonth,
-            })
-            // 當使用者聽完一個音軌 推送Timestamp到firebase 結束 //
-
-// //============================================================================================//
-
-            // 檢查當天日期是否有聽 如果有就上傳資料到user doc 如果沒有就新增 //
-            const checklisten = userRef.collection('Logfile').doc(currentMonth).collection(currentMonth).doc(currentDate)
-            if(checklisten === true){
-                console.log('no checklisten');
-            }else{
-                checklisten.get().then((doc)=>{
-                    const aaa = doc.data().todaytotaltimeplayed
-                    const bbb = parseInt(aaa)+1; ////+1 是因為非同步 舉例:在logfile中總播放次數是2 但是在用戶資料中播放次數是1 所以要同步的話要+1
-                    userRef.set({
-                        currdatetimeplayed: bbb,
-                    }, {merge: true})
-                })
-                .catch((err) => err.message)
+            } else {
             }
-            // 檢查當天日期是否有聽 如果有就上傳資料到user doc 如果沒有就新增 結束 //
+        }).catch(() => {
+        })
 
-// //============================================================================================//
-            
-//             // 當前音軌次數增加 //
-//             userRef.collection('Musics').doc(currplayingmusicid).get().then((doc)=>{
-//                 const a = doc.data().timeplayed;
-//                 const b = parseInt(a)+1;
-//                 userRef.collection('Musics').doc(currplayingmusicid).set({
-//                     timeplayed: b,
-//                     // gamescore: 100,
-//                 })
-//                 // setTimeout(function(){window.location = "/home/game";} ,500)
-//             }).catch((err)=>{
-//                 // userRef.collection('Musics').doc(currplayingmusicid).set({
-//                 //     gamescore: 0,
-//                 // })
-//                 console.log(err.message);
-//             })
-//             // 當前音軌次數增加 結束 //
 
-// //============================================================================================//
-            
-            /// 記錄檔中當月總次數 ///
-            const usermonthlytimes = userRef.collection('Logfile').doc(currentMonth)
-            usermonthlytimes.get().then((doc)=>{
-                const abc = doc.data().currentMonthTotalTimes;
-                const efg = parseInt(abc)+1;
-                usermonthlytimes.update({
-                    currentMonthTotalTimes : efg,
-                })
-            }).catch(()=>{
-                usermonthlytimes.set({
-                    currentMonthTotalTimes : 1,
-                })
+        // Realtime Database
+        const dbRef = firebase.database().ref();
+        dbRef.child("student").child(userId).child("totaltimeplayed").get().then((snapshot) => {
+            const aaa = parseInt(snapshot.val()) + 1;
+            dbRef.child("student").child(userId).update({
+                totaltimeplayed: aaa,
+            });
+        }).catch(() => {
+            firebase.database().ref('student/' + userId).update({
+                totaltimeplayed: 0,
+            });
+        });
+
+
+
+        // 當使用者聽完一個音軌 推送Timestamp到firebase //
+        userRef.update({
+            onlinetime: currentDate,
+            onlinemonth: currentMonth,
+        })
+
+
+        // 檢查當天日期是否有聽 如果有就上傳資料到user doc 如果沒有就新增 //
+        const checklisten = userRef.collection('Logfile').doc(currentMonth).collection(currentMonth).doc(currentDate)
+        if (checklisten === true) {
+            console.log('no checklisten');
+        } else {
+            // checklisten.get().then((doc) => {
+            //     const aaa = doc.data().todaytotaltimeplayed
+            //     const bbb = parseInt(aaa) + 1; ////+1 是因為非同步 舉例:在logfile中總播放次數是2 但是在用戶資料中播放次數是1 所以要同步的話要+1
+            //     userRef.set({
+            //         currdatetimeplayed: bbb,
+            //     }, { merge: true })
+            // })
+            // .catch((err) => err.message)
+        }
+
+
+        /// 記錄檔中當月總次數 ///
+        const usermonthlytimes = userRef.collection('Logfile').doc(currentMonth)
+        usermonthlytimes.get().then((doc) => {
+            const abc = doc.data().currentMonthTotalTimes;
+            const efg = parseInt(abc) + 1;
+            usermonthlytimes.update({
+                currentMonthTotalTimes: efg,
             })
-            /// 記錄檔中當月總次數 結束 ///
-
-// //============================================================================================//
-            
-            // 記錄檔中當天總次數計算 //
-            const userdailytimes = userRef.collection('Logfile').doc(currentMonth).collection(currentMonth).doc(currentDate);
-            userdailytimes.get().then((doc)=>{
-                const abc = doc.data().todaytotaltimeplayed;
-                const efg = parseInt(abc)+1;
-                userdailytimes.update({
-                    todaytotaltimeplayed : efg,
-                })
-            }).catch(()=>{
-                userdailytimes.set({
-                    todaytotaltimeplayed : 1,
-                })
+        }).catch(() => {
+            usermonthlytimes.set({
+                currentMonthTotalTimes: 1,
             })
-            // 記錄檔中當天總次數計算 結束 //
+        })
 
-// //============================================================================================//
-            
-//             // 記錄檔中的當前音軌當天次數增加 //
-//             const logfileRef = userRef.collection('Logfile').doc(currentMonth).collection(currentMonth).doc(currentDate).collection(currplayingmusicid).doc(currplayingmusicid)
-//             logfileRef.get().then((doc)=>{///如果Firebase 有這筆資料播放次數 + 1///
-//                 const a = doc.data().timeplayed;
-//                 const b = parseInt(a)+1;  
-//                 logfileRef.update({
-//                     timeplayed: b,
-//                     gamescore : 100,
-//                 }).then(() => {
-//                     console.log(currentDate, " id: ", currplayingmusicid , 'time played update', b);
-//                 })
-//             }).catch(() => { ///如果Firebase 中沒有這筆資料則新增///
-//                 const newdaytimeplayed = 1;
-//                 const newgamescore = 0;
-//                 logfileRef.set({
-//                     timeplayed: newdaytimeplayed,
-//                     gamescore: newgamescore,
-//                 }).then(() =>{
-//                     console.log(currentDate, "id :", currplayingmusicid ,'new update');
-//                 })
-//             });
-//             // 記錄檔中的當前音軌當天次數增加 結束 //
-                       
-// //============================================================================================//
-
-            // 所有音軌總次數增加 //
-            userRef.get().then((doc)=>{  
-                const c = doc.data().totaltimeplayed;
-                const d = parseInt(c)+1;
-                userRef.update({
-                    totaltimeplayed: d,
-                })
+        // 記錄檔中當天總次數計算 //
+        const userdailytimes = userRef.collection('Logfile').doc(currentMonth).collection(currentMonth).doc(currentDate);
+        userdailytimes.get().then((doc) => {
+            const abc = doc.data().todaytotaltimeplayed;
+            const efg = parseInt(abc) + 1;
+            userdailytimes.update({
+                todaytotaltimeplayed: efg,
             })
-            // 所有音軌總次數增加 結束 //
+        }).catch(() => {
+            userdailytimes.set({
+                todaytotaltimeplayed: 1,
+            })
+        })
 
-// //============================================================================================//
-//         }else{
-//             console.log("update currplayingmusicid failed");
-//         }
-        
+
+        // 所有音軌總次數增加 //
+        // userRef.get().then((doc) => {
+        //     const c = doc.data().totaltimeplayed;
+        //     const d = parseInt(c) + 1;
+        //     userRef.update({
+        //         totaltimeplayed: d,
+        //     })
+        // })
 
     };
 
-    const currentTrack = playlists.findIndex(obj => obj.musicName=== musicName)
+    const currentTrack = playlists.findIndex(obj => obj.musicName === musicName)
     const handleClickNext = () => {
         console.log('Next Track')
         let abc = currentTrack + 1;
-        if ((currentTrack+1)>=playlists.length){
+        if ((currentTrack + 1) >= playlists.length) {
             abc = 0;
         }
         dispatch(setCurrentPlaying(playlists[abc]));
@@ -240,7 +167,7 @@ function FooterMusicPlayer({music}) {
     const handleClickPrev = () => {
         console.log('Previous Track')
         let abc = currentTrack - 1;
-        if ((currentTrack-1)<=-1){
+        if ((currentTrack - 1) <= -1) {
             abc = playlists.length - 1;
         }
         dispatch(setCurrentPlaying(playlists[abc]));
@@ -257,8 +184,8 @@ function FooterMusicPlayer({music}) {
         dispatch(setCurrentPlaying(playlists[abc]));
     }
 
-    
-   
+
+
     // const handleClickNext = () => {
     //     console.log('click next')
     //     let currTrackId = (id+1) % playlists.length;
@@ -275,8 +202,8 @@ function FooterMusicPlayer({music}) {
     //     let currTrackId = (id+1) % playlists.length;
     //     dispatch(setCurrentPlaying(playlists[currTrackId]));
     // }
-    
-    
+
+
     return (
         <div className={"footer-player"}>
             <AudioPlayer
@@ -293,48 +220,48 @@ function FooterMusicPlayer({music}) {
                 customProgressBarSection={
                     [
                         RHAP_UI.CURRENT_TIME,
-                        <Marquee 
-                        pauseOnHover={false}
-                        gradient={true}
-                        gradientWidth={40}
-                        direction='right'
-                        speed={60}
-                    >
-                        <div>
-                            <Name name={"正在收聽的是 : "} className={"marqueenamelabel"} length={bookname.length}/>
-                        </div>
-                        <div>
-                            <Name name={bookname} className={"marqueename"} length={bookname.length}/>
-                        </div>
-                        <div>
-                            <Name name={page} className={"marqueename"} length={page.length}/>
-                        </div>
-                        <div>
-                            <Name name={"請專心聆聽"} className={"marqueenamelabel"} length={page.length}/>
-                        </div>
-                        
-                    </Marquee>,
-                    RHAP_UI.DURATION,
-                ]
+                        <Marquee
+                            pauseOnHover={false}
+                            gradient={true}
+                            gradientWidth={40}
+                            direction='right'
+                            speed={60}
+                        >
+                            <div>
+                                <Name name={"正在收聽的是 : "} className={"marqueenamelabel"} length={bookname.length} />
+                            </div>
+                            <div>
+                                <Name name={bookname} className={"marqueename"} length={bookname.length} />
+                            </div>
+                            <div>
+                                <Name name={page} className={"marqueename"} length={page.length} />
+                            </div>
+                            <div>
+                                <Name name={"請專心聆聽"} className={"marqueenamelabel"} length={page.length} />
+                            </div>
+
+                        </Marquee>,
+                        RHAP_UI.DURATION,
+                    ]
                 }
                 customControlsSection={[
                     RHAP_UI.MAIN_CONTROLS,
                     RHAP_UI.VOLUME_CONTROLS,
                 ]}
-            />   
+            />
             <div>
                 <ToastContainer
-                position="top-center"
-                autoClose={2000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
+                    position="top-center"
+                    autoClose={2000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
                 />
-            </div>       
+            </div>
         </div>
 
     );
--- src/components/fragment/Playlist.jsx
+++ src/components/fragment/Playlist.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import '../assets/scss/Playlist.scss';
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 import MusicCard from "./MusicCard";
 import Container from "./Container";
 import firebase from 'firebase/app';
@@ -8,10 +8,9 @@ import firebase from 'firebase/app';
 const Playlist = () => {
     const [totaltimeplayed, setTotal] = useState();
     const typeOfPlaylist = window.location.pathname.substring(15);
-    const {playlists} = useSelector(state=>state.musicReducer);
-    const dbRef = firebase.database().ref();
-    var userId = firebase.auth().currentUser.uid;
-    dbRef.child("student").child(userId).child("totaltimeplayed").get().then((snapshot) => {
+    const { playlists } = useSelector(state => state.musicReducer);
+    const userId = firebase.auth().currentUser.uid;
+    firebase.database().ref().child("student").child(userId).child("totaltimeplayed").get().then((snapshot) => {
         setTotal(snapshot.val());
     }).catch((error) => {
         console.error(error);
@@ -20,12 +19,12 @@ const Playlist = () => {
         <Container>
             <div className={"Playlist"}>
                 <div className='playlisttitle'> {typeOfPlaylist} </div>
-                <div className='totaltimeplayed'>總聽力次數 : {totaltimeplayed || '---'} 次</div>
+                <div className='totaltimeplayed'>總聽力次數 : {totaltimeplayed || '0'} 次</div>
                 <div className="Playlist-container">
                     {
-                        playlists.map((item)=>(
+                        playlists.map((item) => (
                             item.type === typeOfPlaylist &&
-                            <MusicCard key={item.musicName} music={item}/>
+                            <MusicCard key={item.musicName} music={item} />
                         ))
                     }
                 </div>
