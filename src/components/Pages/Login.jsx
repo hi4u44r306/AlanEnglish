010fc5562d1cb810e69f33b9c44e4e2c26a74dfb
Victor <hi4u44r306@gmail.com>
2024-10-14 00:05:55 +0800

    更改登入規則

--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -261,86 +261,12 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { authentication, rtdb } from "./firebase-config";
-import { child, get, ref, remove, update } from "firebase/database";
+import { child, get, ref, update } from "firebase/database";
 
 function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [isLoading, setIsLoading] = useState(false);
-
-
-    // // 找出有在規定時間內上線的學生
-    // useEffect(() => {
-    //     const db = getFirestore();
-    //     const currentDate = new Date();
-    //     const currentMonthFormatted = currentDate.toJSON().slice(0, 7);
-
-    //     const getStudents = async () => {
-    //         const q = query(collection(db, 'student'),
-    //             where('onlinemonth', '==', currentMonthFormatted),
-    //             where('totaltimeplayed', '>', 0));
-
-    //         try {
-    //             const querySnapshot = await getDocs(q);
-    //             const students = [];
-    //             querySnapshot.forEach((doc) => {
-    //                 students.push(doc.data());
-    //             });
-    //             localStorage.setItem('OnlineStudentData', JSON.stringify(students));
-    //         } catch (error) {
-    //             console.log(error);
-    //         }
-    //     };
-
-    //     getStudents();
-    // }, []);
-
-    // // 找出未上線的學生
-    // useEffect(() => {
-    //     const db = getFirestore();
-
-    //     const date = new Date();
-    //     date.setDate(date.getDate() - 3);
-    //     const offlineLimit = date.toJSON().slice(0, 10);
-
-    //     const getOfflineStudents = async () => {
-    //         const q = query(collection(db, 'student'),
-    //             where('onlinetime', '<=', offlineLimit));
-
-    //         try {
-    //             const querySnapshot = await getDocs(q);
-    //             const students = [];
-    //             querySnapshot.forEach((doc) => {
-    //                 students.push(doc.data());
-    //             });
-    //             localStorage.setItem('OfflineStudentData', JSON.stringify(students));
-    //         } catch (error) {
-    //             console.log(error);
-    //         }
-    //     }
-    //     getOfflineStudents();
-    // }, []);
-
-    // 找出所有學生
-    // useEffect(() => {
-    //     const db = getFirestore();
-
-    //     const getAllStudent = async () => {
-    //         const q = collection(db, 'student');
-
-    //         try {
-    //             const querySnapshot = await getDocs(q);
-    //             const students = [];
-    //             querySnapshot.forEach((doc) => {
-    //                 students.push(doc.data());
-    //             });
-    //             localStorage.setItem('AllStudent', JSON.stringify(students));
-    //         } catch (error) {
-    //             console.log(error);
-    //         }
-    //     }
-    //     getAllStudent();
-    // }, []);
 
     const handleChange = (e) => {
         if (e.target.name === "email") setEmail(e.target.value);
@@ -350,56 +276,6 @@ function Login() {
     const handleKeyDown = (event) => {
         if (event.key === 'Enter' && email && password) login();
     }
-
-    // async function getOnlineStudents() {
-    //     try {
-    //         // Create a reference to the "student" node
-    //         const studentRef = ref(rtdb, '/student');
-
-    //         // Use `orderByChild` and `equalTo` to filter online students
-    //         const onlineStudentsRef = query(studentRef, orderByChild('onlinemonth'), equalTo(new Date().toJSON().slice(0, 7)));
-
-    //         // Create a snapshot listener to get online students
-    //         const unsubscribe = onValue(onlineStudentsRef, (snapshot) => {
-    //             const students = [];
-    //             snapshot.forEach((childSnapshot) => {
-    //                 const data = childSnapshot.val();
-    //                 // Check if totaltimeplayed is greater than 0
-    //                 if (data.totaltimeplayed > 0) {
-    //                     students.push(data);
-    //                 }
-    //             });
-    //             // Store data in localStorage (optional)
-    //             localStorage.setItem('OnlineStudentData', JSON.stringify(students));
-    //         });
-
-    //         // Remember to clean up the listener when necessary
-    //         return unsubscribe; // Return the unsubscribe function for cleanup
-
-    //     } catch (error) {
-    //         console.error("Error getting online students:", error);
-    //     }
-    // }
-
-    // const getOfflineStudents = async () => {
-    //     const db = getFirestore();
-    //     const date = new Date();
-    //     date.setDate(date.getDate() - 3);
-    //     const offlineLimit = date.toJSON().slice(0, 10);
-    //     const q = query(collection(db, 'student'),
-    //         where('onlinetime', '<=', offlineLimit));
-
-    //     try {
-    //         const querySnapshot = await getDocs(q);
-    //         const students = [];
-    //         querySnapshot.forEach((doc) => {
-    //             students.push(doc.data());
-    //         });
-    //         localStorage.setItem('OfflineStudentData', JSON.stringify(students));
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // }
 
     const login = async () => {
         if (!email || !password) {
@@ -430,38 +306,12 @@ function Login() {
             const userCredential = await signInWithEmailAndPassword(authentication, email, password);
             const userid = userCredential.user.uid;
 
-            // const userName = userDoc.data().name.toUpperCase();
-            // const userRef = doc(db, 'student', userCredential.user.uid);
-
-            // const onlinetime = userDoc.data().onlinetime;
-
-            // 更新所有學生資料
-            // const userRTDBRef = ref(rtdb, '/student/' + userCredential.user.uid);
-            // await update(userRTDBRef, {
-            //     Resetallmusic: "2024-03alreadyupdated",
-            //     class: userDoc.data().class,
-            //     Daytotaltimeplayed: userDoc.data().currdatetimeplayed || 0,
-            //     email: userDoc.data().email,
-            //     name: userDoc.data().name,
-            //     onlinemonth: currentMonth,
-            //     onlinetime: currentDate,
-            //     Monthtotaltimeplayed: 0,
-            // })
-
-
-
             const dbRef = ref(rtdb);
             get(child(dbRef, `student/${userid}`)).then((snapshot) => {
                 const userName = snapshot.val().name.toUpperCase();
                 const onlinetime = snapshot.val().onlinetime;
                 const musicRef = ref(rtdb, '/student/' + userid);
                 if (onlinetime !== currentDate) {
-                    // await updateDoc(userRef, {
-                    //     onlinemonth: currentMonth,
-                    //     onlinetime: currentDate,
-                    //     // currdatetimeplayed: 0,
-                    // });
-
                     update(musicRef, {
                         Daytotaltimeplayed: 0,
                         onlinemonth: currentMonth,
@@ -470,23 +320,18 @@ function Login() {
                 }
 
                 if (snapshot.val().Resetallmusic === 'notupdated' || snapshot.val().Resetallmusic !== currentMonth + 'alreadyupdated') {
-                    // setDoc(userRef, {
-                    //     // totaltimeplayed: 0,
-                    //     Resetallmusic: currentMonth + 'alreadyupdated',
-                    // }, { merge: true });
+
                     const databaseRef = ref(rtdb, `student/${userid}`);
                     update(databaseRef, {
                         Monthtotaltimeplayed: 0,
                         Resetallmusic: currentMonth + 'alreadyupdated',
                     });
-                    const musicLogfileRef = ref(rtdb, `student/${userid}/MusicLogfile`);
-                    remove(musicLogfileRef);
+
+                    // 這個是播放紀錄不能刪
+                    // const musicLogfileRef = ref(rtdb, `student/${userid}/MusicLogfile`);
+                    // remove(musicLogfileRef);
                 }
 
-                // if (snapshot.val().class === 'Teacher') {
-                //     getOnlineStudents();
-                //     getOfflineStudents();
-                // }
                 toast.promise(
                     new Promise(resolve => setTimeout(resolve, 500)),
                     {
