c219f307fd5261177a845ada37f71eb2967067f6
Victor <hi4u44r306@gmail.com>
2025-09-23 11:28:19 +0800

    add links

--- src/components/Pages/Links.jsx
+++ src/components/Pages/Links.jsx
@@ -2,21 +2,31 @@ import React from 'react';
 import './css/Links.scss';
 
 const links = [
-    { title: "習作本 1", url: "https://www.youtube.com/channel/UC1" },
-    { title: "習作本 2", url: "https://www.youtube.com/channel/UC2" },
-    { title: "習作本 3", url: "https://www.youtube.com/channel/UC3" },
-    { title: "習作本 4", url: "https://www.youtube.com/channel/UC4" },
-    { title: "習作本 5", url: "https://www.youtube.com/channel/UC5" },
-    { title: "習作本 6", url: "https://www.youtube.com/channel/UC6" },
+    { title: "習作本 3", url: "https://www.youtube.com/watch?v=zqgkmTWoTYc" },
+    { title: "習作本 4", url: "https://youtu.be/Dr2G4S_0Htw?si=h0w_7rkjj0d4wQHd" },
+    { title: "F1", url: "https://youtu.be/hl-NknRrMwU?si=KfQ5Uh_2Mr7pO1d5" },
+    { title: "F2", url: "https://youtu.be/CIUyObcfZGA?si=ToCwAKtRBe3HQ0sS" },
 ];
 function Links() {
     return (
         <div className="Links">
             <header className="links-header">
-                <div className="logo">Alan English</div>
+                <div className="loginbrand">
+                    <span>A</span>
+                    <span>L</span>
+                    <span>A</span>
+                    <span>N</span>
+                    <span> </span>
+                    <span>E</span>
+                    <span>N</span>
+                    <span>G</span>
+                    <span>L</span>
+                    <span>I</span>
+                    <span>S</span>
+                    <span>H</span>
+                </div>
             </header>
             <main className="links-main">
-                <h1>習作本連結</h1>
                 <div className="button-grid">
                     {links.map((link, index) => (
                         <a
