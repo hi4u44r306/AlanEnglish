3d38db2447f7dd9e5784d5c77343e310eeea0584
Victor <65693742+hi4u44r306@users.noreply.github.com>
2026-08-13 07:49:50 +0800

    Refresh showcase navigation

--- src/components/fragment/ShowcaseNavbar.jsx
+++ src/components/fragment/ShowcaseNavbar.jsx
@@ -1,75 +1,93 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useEffect, useState } from 'react';
-import { Container, Nav, NavLink, Navbar, Offcanvas } from 'react-bootstrap';
-import Brand from './Brand';
-import Menu from '../assets/img/menu.png';
-import '../assets/scss/ShowcaseNavbar.scss';
-import { BiLogIn } from 'react-icons/bi'
-import { Link } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useEffect, useState } from "react";
+import { Container, Nav, Navbar, Offcanvas } from "react-bootstrap";
+import { BiChevronRight, BiLogIn } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import Brand from "./Brand";
+import Menu from "../assets/img/menu.png";
+import "../assets/scss/ShowcaseNavbar.scss";
 
 function ShowcaseNavbar({ nav1, nav2, nav3, nav4 }) {
     const [scrolled, setScrolled] = useState(false);
 
-    // Function to handle the scroll event
-    const handleScroll = () => {
-        if (window.scrollY > 100) {
-            setScrolled(true);
-        } else {
-            setScrolled(false);
-        }
-    };
-
     useEffect(() => {
-        // Add a scroll event listener when the component mounts
-        window.addEventListener('scroll', handleScroll);
+        const handleScroll = () => {
+            setScrolled(window.scrollY > 20);
+        };
 
-        // Remove the scroll event listener when the component unmounts
+        handleScroll();
+        window.addEventListener("scroll", handleScroll, { passive: true });
+
         return () => {
-            window.removeEventListener('scroll', handleScroll);
+            window.removeEventListener("scroll", handleScroll);
         };
     }, []);
 
+    const navItems = [
+        { label: "關於 AE", href: nav1 },
+        { label: "學習方式", href: nav2 },
+        { label: "平台特色", href: nav3 },
+        { label: "開始學習", href: nav4 }
+    ];
+
     return (
-        <>
-            {['xl'].map((expand) => (
-                <Navbar collapseOnSelect={true} key={expand} expand={expand} className={`showcasenavbar ${scrolled ? 'scrolled' : ''}`}>
-                    <Container>
-                        <Navbar.Brand className="brand">
-                            <Brand />
-                        </Navbar.Brand>
-                        <Navbar.Toggle className="toggle" aria-controls={`offcanvasNavbar-expand-${expand}`}>
-                            <img style={{ width: 30, height: 30 }} src={Menu} alt="menu" />
-                        </Navbar.Toggle>
-                        <Navbar.Offcanvas
-                            id={`offcanvasNavbar-expand-${expand}`}
-                            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-                            placement="end"
+        <Navbar
+            expand="xl"
+            className={`showcase-navbar ${scrolled ? "is-scrolled" : ""}`}
+        >
+            <Container className="showcase-navbar-container">
+                <Navbar.Brand className="showcase-navbar-brand" as={Link} to="/showcase">
+                    <Brand />
+                </Navbar.Brand>
+
+                <Navbar.Toggle
+                    className="showcase-navbar-toggle"
+                    aria-controls="showcase-navbar-offcanvas"
+                >
+                    <img src={Menu} alt="開啟選單" />
+                </Navbar.Toggle>
+
+                <Navbar.Offcanvas
+                    id="showcase-navbar-offcanvas"
+                    aria-labelledby="showcase-navbar-offcanvas-label"
+                    placement="end"
+                >
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title
+                            className="showcase-navbar-offcanvas-title"
+                            id="showcase-navbar-offcanvas-label"
                         >
-                            <Offcanvas.Header closeButton>
-                                <Offcanvas.Title className="brand" id={`offcanvasNavbarLabel-expand-${expand}`}>
-                                    <Brand />
-                                </Offcanvas.Title>
-                            </Offcanvas.Header>
-                            <Offcanvas.Body className="navbackground">
-                                <Nav className={`showcasenavbar ${scrolled ? 'scrolled' : ''}`}>
-                                    <NavLink className='showcasenavlink' href={nav1}>{nav1.slice(1)}</NavLink>
-                                    <NavLink className='showcasenavlink' href={nav2}>{nav2.slice(1)}</NavLink>
-                                    <NavLink className='showcasenavlink' href={nav3}>{nav3.slice(1)}</NavLink>
-                                    <NavLink className='showcasenavlink' href={nav4}>{nav4.slice(1)}</NavLink>
-                                    <NavLink className='showcase-loginbtn' as={Link} href="/" to="/">
-                                        <BiLogIn />
-                                        Login
-                                    </NavLink>
-                                    <NavLink className='showcase-tryforfreebtn' as={Link} href="/freetrial" to="/freetrial">
-                                        開通帳號
-                                    </NavLink>
-                                </Nav>
-                            </Offcanvas.Body>
-                        </Navbar.Offcanvas>
-                    </Container>
-                </Navbar>
-            ))}
-        </>
+                            <Brand />
+                        </Offcanvas.Title>
+                    </Offcanvas.Header>
+
+                    <Offcanvas.Body>
+                        <Nav className="showcase-navbar-links ms-auto">
+                            {navItems.map((item) => (
+                                <Nav.Link
+                                    key={item.label}
+                                    className="showcase-navbar-link"
+                                    href={item.href}
+                                >
+                                    {item.label}
+                                </Nav.Link>
+                            ))}
+                        </Nav>
+
+                        <div className="showcase-navbar-actions">
+                            <Link className="showcase-navbar-login" to="/">
+                                <BiLogIn />
+                                登入
+                            </Link>
+                            <Link className="showcase-navbar-trial" to="/freetrial">
+                                開通帳號
+                                <BiChevronRight />
+                            </Link>
+                        </div>
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
+            </Container>
+        </Navbar>
     );
 }
 
