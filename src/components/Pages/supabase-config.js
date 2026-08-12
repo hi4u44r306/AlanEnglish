02df14b421dfa29fbac7b4d9144fde724af35662
Victor <65693742+hi4u44r306@users.noreply.github.com>
2026-08-12 19:18:32 +0800

    export supabase endpoint settings

--- src/components/Pages/supabase-config.js
+++ src/components/Pages/supabase-config.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://rszabtuzsvhaooajnvgc.supabase.co";
-const supabaseKey = "sb_publishable_ka6h_OH9MQC49oSDNJJ8Mg_umgajkLd";
+export const supabaseUrl = "https://rszabtuzsvhaooajnvgc.supabase.co";
+export const supabaseKey = "sb_publishable_ka6h_OH9MQC49oSDNJJ8Mg_umgajkLd";
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
     auth: {
