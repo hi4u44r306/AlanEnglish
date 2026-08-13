95c1920dcb6ec8343229706a2f5469aca9a8527f
Victor <65693742+hi4u44r306@users.noreply.github.com>
2026-08-13 20:35:47 +0800

    add music library manager shell

---
+++ src/components/Pages/MusicLibraryManager.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MusicLibraryManager() {
+    return (
+        <div>
+            <h1>管理音檔</h1>
+        </div>
+    );
+}
+
+export default MusicLibraryManager;
